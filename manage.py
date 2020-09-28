9986189d5afc6e164a958a1ed89e8bba9cc7f419
Balvine <balvy075@gmail.com>
2020-09-28 19:21:15 +0300

    added navbar template

--- manage.py
+++ manage.py
@@ -1,16 +1,30 @@
-from app import create_app, db
-from flask_script import Manager,server
+from app import create_app,db
+from flask_script import Manager,Server
+from  flask_migrate import Migrate, MigrateCommand
 from app.models import User
-from flask_migrate import Migrate, MigrateCommand
 
 
+# Creating app instance
+# app = create_app('test')
+app=create_app('development')
+
+manager=Manager(app)
+manager.add_command('server',Server)
+
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
 
 
+#use the manager shell decorator to access the shell on the command line
 @manager.shell
 def make_shell_context():
-    return dict(app=app, db=db, User=User, Comment=Comment, Post=Post)
-
-
-if __name__ == '__main__':
+    return dict(app = app,db = db,User = User )
     
+@manager.command
+def test():
+    import unittest
+    tests=unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+    
+if __name__=='__main__':
     manager.run()
