f71063df81931a681630d496535119ab7b9c05c3
Balvine <balvy075@gmail.com>
2020-09-29 09:06:13 +0300

    added test folder

--- manage.py
+++ manage.py
@@ -1,30 +1,28 @@
-from app import create_app,db
+
 from flask_script import Manager,Server
+from app import create_app,db
+from app.models import User,Blog,Comment,Subscribe
 from  flask_migrate import Migrate, MigrateCommand
-# from app.models import User
 
 
 # Creating app instance
-# app = create_app('test')
-app=create_app('development')
-
-manager=Manager(app)
-manager.add_command('server',Server)
-
+# app = create_app('development')
+app = create_app('development')
+manager = Manager(app)
 migrate = Migrate(app,db)
+manager.add_command('server',Server)
 manager.add_command('db',MigrateCommand)
 
-
-#use the manager shell decorator to access the shell on the command line
-@manager.shell
-def make_shell_context():
-    return dict(app = app,db = db,User = User )
-    
 @manager.command
 def test():
+    """Run the unit tests."""
     import unittest
-    tests=unittest.TestLoader().discover('tests')
+    tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
-    
-if __name__=='__main__':
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User,Blog=Blog,Comment=Comment,Subscribe=Subscribe)
+if __name__ == '__main__':
     manager.run()
+
+   
