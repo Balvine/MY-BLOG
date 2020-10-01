dc54faf3321db3f4cd6107cd24c2ce8dad5ec84a
Balvine <balvy075@gmail.com>
2020-10-01 12:10:16 +0300

    added error file

---
+++ app/main/error.py
@@ -0,0 +1,5 @@
+{% extends 'base.html'%}
+
+{% block content %}
+    <h1>ooops! you are lost</h1>
+{% endblock %}
--- test/test_user.py
+++ test/test_user.py
@@ -4,7 +4,7 @@ from app.models import User
 class UserModelTest(unittest.TestCase):
 
     def setUp(self):
-        self.new_user = User(password = 'nyururu')
+        self.new_user = User(password = 'barl')
 
     def test_password_setter(self):
         self.assertTrue(self.new_user.pass_secure is not None)
@@ -14,4 +14,4 @@ class UserModelTest(unittest.TestCase):
                 self.new_user.password
 
     def test_password_verification(self):
-        self.assertTrue(self.new_user.verify_password('nyururu'))
+        self.assertTrue(self.new_user.verify_password('barl'))
